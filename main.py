382bd57c056e1a0dd522917c01552147b41b02bd
Art3mLapa <76118653+Art3mLapa@users.noreply.github.com>
2023-07-28 18:17:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,6 @@ from colorama import Fore, Style
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
     
-def menu():
-    print(Fore.RED + '''
-______ _____    _____       __                  
-| ___ \  __ \  /  ___|     /  |                 
-| |_/ / |  \/  \ `--. _ __ `| | _ __   ___ _ __ 
-|  __/| | __    `--. \ '_ \ | || '_ \ / _ \ '__|
-| |   | |_\ \  /\__/ / | | || || |_) |  __/ |_  
-\_|    \____/  \____/|_| |_\___/ .__/ \___|_(_) 
-                               | |              
-                               |_|              1.0 by Art3mLapa.
-                ''' + Style.RESET_ALL)
-
 def generate_random_number(min_length, max_length):
     length = random.randint(min_length, max_length)
     return ''.join(str(random.randint(0, 9)) for _ in range(length))
@@ -41,12 +29,10 @@ def check_inventory_link(inventory_url, random_number):
 def generate_and_check_links():
     with open("webhook.txt", "r") as file:
         webhook_url = file.read().strip()
-
+        itemID = input("Item(ID) > ")
+        
     while True:
-        clear_console()
-        menu()
-        itemID = input(Fore.YELLOW + "Item(ID) > ")
-        random_number = generate_random_number(7, 8) # minimum, maximum ID length
+        random_number = generate_random_number(8, 9)
         profile_url = f"https://www.roblox.com/users/{random_number}/profile"
         if not check_profile_link(profile_url, random_number):
             continue
